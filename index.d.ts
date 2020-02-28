20b95c2fd1087402d94570025b48885ddc9c4731
Alex <alex.hill@gmail.com>
2020-02-28 17:23:22 +0000

    this apparently works :shrug:

--- index.d.ts
+++ index.d.ts
@@ -1,80 +1,80 @@
 declare module "@reside-ic/vue-dynamic-form" {
-    import Vue from "vue";
-
-    export type DynamicControlSection = {
-        label: string
-        description?: string
-        controlGroups: DynamicControlGroup[]
-    }
-
-    export type DynamicControlGroup = {
-        label?: string
-        controls: Control[]
-    }
-
-    export type Option = {
-        id: string,
-        label: string,
-        children?: Option[]
-    }
-
-    export type DynamicControlType = "multiselect" | "select" | "number"
-    export type Control = SelectControl | MultiSelectControl | NumberControl
-
-    export type DynamicControl = {
-        name: string,
-        label?: string,
-        type: DynamicControlType
-        required: boolean
-        helpText?: string
-        value?: string | string[] | number | null
-    }
-
-    export type SelectControl = DynamicControl & {
-        options: Option[]
-        value?: string | null
-    }
-
-    export type MultiSelectControl = DynamicControl & {
-        options: Option[]
-        value?: string[] | string
-    }
-
-    export type NumberControl = DynamicControl & {
-        min?: number
-        max?: number
-        value?: number | null
-    }
-
-    export type DynamicFormMeta = {
-        controlSections: DynamicControlSection[]
-    }
-
-    type Dict<V> = { [k: string]: V }
-
-    export type DynamicFormData = Dict<string | string[] | number | null>
-
-    export class DynamicForm extends Vue {
-        formMeta: DynamicFormMeta;
-        includeSubmitButton?: boolean;
-        submitText?: string;
-        id?: string;
-    }
-
-    export function isControl(object: any): Boolean
-
-    export function isNumberControl(object: any): object is NumberControl
-
-    export function isSelectControl(object: any): object is SelectControl
-
-    export function isMultiSelectControl(object: any): object is MultiSelectControl
-
-    export function isSelectOption(object: any): object is Option
-
-    export function isDynamicControlGroup(object: any): object is DynamicControlGroup
-
-    export function isDynamicControlSection(object: any): object is DynamicControlSection
-
-    export function isDynamicFormMeta(object: any): object is DynamicFormMeta
-
 }
+
+import Vue from "vue";
+
+export type DynamicControlSection = {
+    label: string
+    description?: string
+    controlGroups: DynamicControlGroup[]
+}
+
+export type DynamicControlGroup = {
+    label?: string
+    controls: Control[]
+}
+
+export type Option = {
+    id: string,
+    label: string,
+    children?: Option[]
+}
+
+export type DynamicControlType = "multiselect" | "select" | "number"
+export type Control = SelectControl | MultiSelectControl | NumberControl
+
+export type DynamicControl = {
+    name: string,
+    label?: string,
+    type: DynamicControlType
+    required: boolean
+    helpText?: string
+    value?: string | string[] | number | null
+}
+
+export type SelectControl = DynamicControl & {
+    options: Option[]
+    value?: string | null
+}
+
+export type MultiSelectControl = DynamicControl & {
+    options: Option[]
+    value?: string[] | string
+}
+
+export type NumberControl = DynamicControl & {
+    min?: number
+    max?: number
+    value?: number | null
+}
+
+export type DynamicFormMeta = {
+    controlSections: DynamicControlSection[]
+}
+
+type Dict<V> = { [k: string]: V }
+
+export type DynamicFormData = Dict<string | string[] | number | null>
+
+export declare class DynamicForm extends Vue {
+    formMeta: DynamicFormMeta;
+    includeSubmitButton?: boolean;
+    submitText?: string;
+    id?: string;
+}
+
+export declare function isControl(object: any): Boolean
+
+export declare function isNumberControl(object: any): object is NumberControl
+
+export declare function isSelectControl(object: any): object is SelectControl
+
+export declare function isMultiSelectControl(object: any): object is MultiSelectControl
+
+export declare function isSelectOption(object: any): object is Option
+
+export declare function isDynamicControlGroup(object: any): object is DynamicControlGroup
+
+export declare function isDynamicControlSection(object: any): object is DynamicControlSection
+
+export declare function isDynamicFormMeta(object: any): object is DynamicFormMeta
